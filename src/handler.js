a6031bb564215b7fd5b3e96db5fff0de92f12207
Miguel Santana <miguel@savesolarcorporation.com>
2023-01-19 01:56:22 -0500

    add serverless-iam-roles-per-function plugin

--- src/handler.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports.titanLogic = (event) => {
-    const randomNumber = parseInt(Math.random() * 100)
-    console.log(`The random number is: ${randomNumber}`)
-    return randomNumber
-}
